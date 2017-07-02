8eda3dcf209fa563c89bcbdc2913cae0e5f23160
Alexander Mykolaichuk <alexmix3000@gmail.com>
2017-07-02 11:09:12 +0300

    add edit mutation

--- src/schema.js
+++ src/schema.js
@@ -80,6 +80,18 @@ const mutation = new GraphQLObjectType({
       resolve(parentValue, { id }) {
         return axios.delete(`http://localhost:3000/users/${id}`).then(res => res.data);
       }
+    },
+    editUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        companyId: { type: GraphQLString },
+      },
+      resolve(parentValue, { id, firstName, age, companyId }) {
+        return axios.patch(`http://localhost:3000/users/${id}`, {firstName, age, companyId}).then(res => res.data);
+      }
     }
   }
 });
