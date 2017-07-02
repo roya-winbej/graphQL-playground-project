3af190279e8b0edb94aa80f26c7022afe6498fd0
Alexander Mykolaichuk <alexmix3000@gmail.com>
2017-07-02 21:21:23 +0300

    add likes

--- src/Lyrical-GraphQL/client/components/LyricList.js
+++ src/Lyrical-GraphQL/client/components/LyricList.js
@@ -1,13 +1,28 @@
 import React, {Component} from 'react';
+import { graphql } from 'react-apollo';
+import  addLikeQuery from '../queries/addLikeToLyric';
 
 class LyricList extends Component {
+
+  onLike(id) {
+    this.props.mutate({
+      variables: {
+        id
+      }
+    });
+  }
+
   render() {
     return (
       <div>
         <h6>Lyric list</h6>
         <ul className="collection">
-          {this.props.lyrics.map(({id, content}) => (
-            <li className="collection-item" key={id}>{content}</li>
+          {this.props.lyrics.map(({id, content, likes }) => (
+            <li className="collection-item" key={id} onClick={() => this.onLike(id)}>
+              <i className="material-icons right">thumb_up</i>
+              <span className="right">{likes}</span>
+              {content}
+            </li>
           ))}
         </ul>
       </div>
@@ -15,4 +30,4 @@ class LyricList extends Component {
   }
 }
 
-export default LyricList;
+export default graphql(addLikeQuery)(LyricList);
